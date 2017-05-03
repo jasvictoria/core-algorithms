2fa7a845a88467458c770fd399dbcdf605a74afb
jasvictoria <jasvmurray@gmail.com>
2017-05-03 09:43:04 -0700

    add palindrome

---
+++ src/palindrome.js
@@ -0,0 +1,5 @@
+export default function isPalindrome(string) {
+  let newString = string.toLowerCase().replace(/[^a-z]+/ig, '');
+  let reverseString = newString.split('').reverse().join('')
+    return reverseString === newString
+}
